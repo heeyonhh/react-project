50eb53b3653ab684b92ef63e0bfd5ccda256e011
heeyonhh <lightram23@gmail.com>
2023-08-01 20:41:44 +0900

    md 2

--- src/components/Product.js
+++ src/components/Product.js
@@ -4,14 +4,16 @@ import Box from '@mui/material/Box';
 
 // 메인 추천 메뉴
 function Product(props) {
-    return (
-      <Box className='md'>
+  return (
+    <Box className='md'>
+      <div className='productimgwrap'>
         <img className="productimg" src={'/img/productimg' + (props.i + 1) + '.png'} />
-        <h4 className='title'>{props.product.title}</h4>
-        <p className='content'>{props.product.content}</p>
-        <p className='price'>{props.product.price}</p>
-      </Box>
-    )
-  };
+      </div>
+      <h4 className='title'>{props.product.title}</h4>
+      <p className='content'>{props.product.content}</p>
+      <p className='price'>{props.product.price}</p>
+    </Box>
+  )
+};
 
 export default Product;
--- src/dataproduct.js
+++ src/dataproduct.js
@@ -4,21 +4,21 @@ let productdata = [
         id: 0,
         title: "생과일 수박 주스",
         content: "ICED Only",
-        price: 4900
+        price: "4900원"
     },
 
     {
         id: 1,
         title: "연유 콜드브루",
         content: "ICED Only",
-        price: 4700
+        price: "4700원"
     },
 
     {
         id: 2,
         title: "달고나 라떼",
-        content: "HOT & ICED",
-        price: 3700
+        content: null,
+        price: "3700원"
     }
 ];
 
