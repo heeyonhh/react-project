d8602bcc382006b09a9a5e603790fd81da7ee620
heeyonhh <lightram23@gmail.com>
2023-08-02 10:48:51 +0900

    md

--- src/dataproduct.js
+++ src/dataproduct.js
@@ -4,22 +4,43 @@ let productdata = [
         id: 0,
         title: "생과일 수박 주스",
         content: "ICED Only",
-        price: "4900원"
+        price: "4,900원"
     },
 
     {
         id: 1,
         title: "연유 콜드브루",
         content: "ICED Only",
-        price: "4700원"
+        price: "4,700원"
     },
 
     {
         id: 2,
         title: "달고나 라떼",
         content: null,
-        price: "3700원"
-    }
+        price: "3,700원"
+    },
+
+    {
+        id: 3,
+        title: "흑당 콜드브루",
+        content: "ICED Only",
+        price: "4,700원"
+    },
+
+    {
+        id: 4,
+        title: "피스타치오 쉐이크",
+        content: "ICED Only",
+        price: "5,200원"
+    },
+
+    {
+        id: 5,
+        title: "더블베리 요거놀라 1인빙수",
+        content: null,
+        price: "6,300원"
+    },
 ];
 
 export default productdata;
