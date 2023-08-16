b085b866d19f3c4bd22446dfe751b40a29d68caa
heeyonhh <lightram23@gmail.com>
2023-08-16 17:50:12 +0900

    .env

--- src/route/Login.js
+++ src/route/Login.js
@@ -1,10 +1,7 @@
-import KakaoLogin from 'react-kakao-login';
-import REACT_APP_KAKAO_API_KEY from '../key.env';
-import REACT_APP_KAKAO_CLIENT_ID from '../key.env';
-
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Checkbox from '@mui/material/Checkbox';
+import Button from "@mui/material/Button";
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Link from "@mui/material/Link";
 import Typography from '@mui/material/Typography';
@@ -12,20 +9,23 @@ import Typography from '@mui/material/Typography';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 
 function Login() {
+    const KAKAO_API_KEY = process.env.REACT_APP_KAKAO_API_KEY;
+    const KAKAO_CLIENT_ID = process.env.REACT_APP_KAKAO_CLIENT_ID;
+
     // SDK는 한 번만 초기화
     if (!window.Kakao.isInitialized()) {
         // JavaScript key를 인자로 주고 SDK 초기화
-        window.Kakao.init(REACT_APP_KAKAO_API_KEY);
+        window.Kakao.init(KAKAO_API_KEY);
     }
 
     const handleKakaoLogin = () => {
         // kakao.auth.login 함수를 사용하여 로그인 처리
         window.Kakao.Auth.login({
-            success: function(authObj) {
+            success: function (authObj) {
                 console.log('카카오 로그인 성공:', authObj);
                 // 성공 콜백에서 필요한 처리를 하세요
             },
-            fail: function(error) {
+            fail: function (error) {
                 console.log('카카오 로그인 실패:', error);
                 // 실패 콜백에서 에러 처리를 하세요
             },
@@ -48,7 +48,10 @@ function Login() {
                     로그인
                 </Typography>
 
-                <button token={REACT_APP_KAKAO_CLIENT_ID} onClick={handleKakaoLogin}>카카오로 회원가입/로그인</button>
+                <Button onClick={handleKakaoLogin} data-token={KAKAO_CLIENT_ID}>
+                    카카오로 회원가입/로그인
+                </Button>
+
 
                 {/* 이메일 비밀번호 기억 폼 */}
                 <FormControlLabel
---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const webpack = require("webpack");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+module.exports = {
+  plugins: [
+    // dotenv 사용을 위한 설정
+    new webpack.DefinePlugin({
+      'process.env': JSON.stringify(process.env),
+    })
+  ]
+};
